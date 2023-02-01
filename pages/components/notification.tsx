61c55b497691adcb361244cf4279b794c2ea52e2
Benjamin Schodts <codingraine@gmail.com>
2023-02-01 14:02:00 +0100

    update notification

--- pages/components/notification.tsx
+++ pages/components/notification.tsx
@@ -3,13 +3,13 @@ import styles from '../../styles/Notification.module.scss';
 export default function Home({ notification }: any) {
 	return (
 		<div className={styles.notificationWrapper}>
-			<p className={styles.notificationText}>{notification.text}</p>
-			{notification.buttonText && notification.buttonLink ? (
+			<p className={styles.notificationText}>{notification?.text}</p>
+			{notification?.buttonText && notification?.buttonLink ? (
 				<a
-					href={notification.buttonLink}
+					href={notification?.buttonLink}
 					className={styles.notificationButton}
 				>
-					{notification.buttonText}
+					{notification?.buttonText}
 				</a>
 			) : null}
 		</div>
